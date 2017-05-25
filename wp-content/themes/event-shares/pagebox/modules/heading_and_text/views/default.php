200555e681dafb703bc80e0c293575df6fa294e6
Przemek <przemyslawkudla94@gmail.com>
2017-05-25 10:42:59 +0200

    Added 'Heading and text' module.

---
+++ wp-content/themes/event-shares/pagebox/modules/heading_and_text/views/default.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Use this for PhpStorm hints.
+ * @var \Nurture\Pagebox\Module\Scope $this
+ * @var \Nurture\EventShares\Module\HeadingAndTExt $module
+ */
+
+$module = $this->getModule();
+?>
+<div class="<?= $module->getClass() ?>">
+    <div class="container-fluid second-background-color">
+        <div class="container" id="heading-and-text">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4>firm partnership</h4>
+                </div>
+                <div class="col-lg-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel
+                        efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget
+                        nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at.
+                        Nam varius eleifend risus ac aliquam. Sed semper turpis enim, non dignissim lorem convallis
+                        vitae.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
