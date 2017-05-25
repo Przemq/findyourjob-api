278671b234d3d20d834d53d6a49840fe5e9c7063
Przemek <przemyslawkudla94@gmail.com>
2017-05-25 12:17:59 +0200

    Adding .gitkeep file to all modules. Some frontend modification in 'Text banner $ subnav' module

--- wp-content/themes/event-shares/pagebox/modules/text_banner_and_subnav/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/text_banner_and_subnav/views/default.php
@@ -15,8 +15,6 @@ $module = $this->getModule();
             <div class="row">
                 <div class="col-lg-12">
 
-
-                    <!-- Nav tabs -->
                     <ul class="nav nav-tabs hidden-sm-down" role="tablist" style="border-bottom-color: black">
                         <li class="nav-item single-li-item">
                             <a class="nav-link active single-link" data-toggle="tab" href="#our-firm" role="tab" >Our Firm</a>
@@ -38,15 +36,11 @@ $module = $this->getModule();
                         </li>
                     </ul>
 
-                    <!-- Tab panes -->
                     <div class="tab-content">
                         <div class="tab-pane active" id="our-firm" role="tabpanel"><p class="col-lg-8 offset-lg-2 offset-sm-0">1 Lorem ipsum dolor quam, sit amet, vitae et ullamcorper, vitae  netus neque morbi mauris sed vel tempor.</p></div>
-                        <div class="tab-pane" id="the-team" role="tabpanel"><p class="col-lg-8 offset-lg-2 offset-sm-0">2 Lorem ipsum dolor sit amet, duis vitae et ullamcorper,, quam, neque morbi mauris sed vel tempor.</p></div>
-                        <div class="tab-pane" id="etf-education" role="tabpanel"><p class="col-lg-8 offset-lg-2 offset-sm-0">3 Lorem ipsum dolor si quam,t amet, duis vitae et ullamquam, corper, vitae neque morbi mauris sed vel tempor.</p></div>
+                        <div class="tab-pane" id="the-team" role="tabpanel"><p class="col-lg-8 offset-lg-2 offset-sm-0">2 Lorem ipsum dolor sit amet, duis vitae et ullamcorper, quam, neque morbi mauris sed vel tempor.</p></div>
+                        <div class="tab-pane" id="etf-education" role="tabpanel"><p class="col-lg-8 offset-lg-2 offset-sm-0">3 Lorem ipsum dolor si quam,t amet, duis vitae et ullamquam, corper, morbi mauris sed vel tempor.</p></div>
                     </div>
-
-
-
                 </div>
             </div>
         </div>
