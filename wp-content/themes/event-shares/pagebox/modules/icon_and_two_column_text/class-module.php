8634781e110320eaf643f0dcb3cd53604c0d3303
Piotr <pgurdek@gmail.com>
2017-06-06 12:20:33 +0200

    EV-18 code update

--- wp-content/themes/event-shares/pagebox/modules/icon_and_two_column_text/class-module.php
+++ wp-content/themes/event-shares/pagebox/modules/icon_and_two_column_text/class-module.php
@@ -11,37 +11,98 @@ use Nurture\Pagebox\Module\View\StaticCacheInterface;
 
 class IconWithTwoColumnText extends AbstractModule implements StaticCacheInterface {
 
-    /**
-     * Module config
-     * @return array Module configuration.
-     */
-    protected function config()
-    {
-        return [
-        	'version'       => '1.0.0',
-            'title'         => 'Icon And Two column Text',
-            'description'   => 'Icon and two column witch text'
-        ];
-    }
+	/**
+	 * Module config
+	 * @return array Module configuration.
+	 */
+	protected function config() {
+		return [
+			'version'     => '1.0.0',
+			'title'       => 'Icon And Two column Text',
+			'description' => 'Icon and two column witch text'
+		];
+	}
 
-    /**
-     * @return array Fields configuration.
-     */
-    protected function fields()
-    {
-        return [
-	        //Title
-	        'title' => [
-		        'type' => 'input:text',
-		        'label' => 'Title',
-		        'description' => 'Please enter title'
-	        ],
-	        'titleColor' => [
-		        'type' => 'input:color',
-		        'label' => 'Title color',
-		        'default' => '#282780',
-		        'sass' => true
-	        ],
-        ];
-    }
+	/**
+	 * @return array Fields configuration.
+	 */
+	protected function fields() {
+		return [
+			//Title
+			'title'            => [
+				'type'        => 'input:text',
+				'label'       => 'Title',
+				'description' => 'Please enter title'
+			],
+			'titleColor'       => [
+				'type'    => 'input:color',
+				'label'   => 'Title color',
+				'default' => '#282780',
+				'sass'    => true
+			],
+			'titleSize'        => [
+				'type'    => 'input:text',
+				'label'   => 'Title size px()',
+				'default' => '25px',
+				'sass'    => true
+			],
+			'iconColor'        => [
+				'type'    => 'input:color',
+				'label'   => 'Icon color',
+				'default' => '#59C1A2',
+				'sass'    => true
+			],
+			'description'      => [
+				'type'    => 'editor',
+				'label'   => 'Description',
+				'default' => 'Please enter description'
+			],
+			'descriptionColor' => [
+				'type'    => 'input:color',
+				'label'   => 'Description color',
+				'default' => '#000000',
+				'sass'    => true
+			],
+			'descriptionSize'  => [
+				'type'    => 'input:text',
+				'label'   => 'Description size px()',
+				'default' => '14px',
+				'sass'    => true
+			],
+
+
+			'rows'            => [
+				'type'   => 'repeater',
+				'label'  => 'Table',
+				'fields' => [
+					'rowBoldText' => [
+						'type'  => 'input:text',
+						'label' => 'Enter row left text',
+					],
+					'rowText'     => [
+						'type'  => 'input:text',
+						'label' => 'Enter row right text ',
+					],
+				],
+			],
+			'rowColor'        => [
+				'type'    => 'input:color',
+				'label'   => 'Row color',
+				'default' => '#1B1B1B',
+				'sass'    => true
+			],
+			'rowSize'         => [
+				'type'    => 'input:text',
+				'label'   => 'Row size px()',
+				'default' => '14px',
+				'sass'    => true
+			],
+			'backgroundColor' => [
+				'type'    => 'input:color',
+				'label'   => 'Background color',
+				'default' => '#EBEBEB',
+				'sass'    => true
+			],
+		];
+	}
 }
--- wp-content/themes/event-shares/pagebox/modules/icon_and_two_column_text/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/icon_and_two_column_text/views/default.php
@@ -6,46 +6,42 @@
  * @var \Nurture\EventShares\Module\IconWithTwoColumnText $module
  */
 
-$module = $this->getModule();
+$module      = $this->getModule();
+$title       = $this->getInput( 'title' );
+$description = $this->getEditor( 'description' )->getContent();
+$table       = $this->getEditor( 'description' )->getContent();
 ?>
 <div class="<?= $module->getClass() ?>">
-    <div class="container"><?php include_once('pageContent.php'); ?> <a class="task-number" target="_blank"
-                                                                        href="https://nurture.atlassian.net/browse/EV-18">EV-18</a>
+    <div class="container"><?php include_once( 'pageContent.php' ); ?> <a class="task-number" target="_blank"
+                                                                          href="https://nurture.atlassian.net/browse/EV-18">EV-18</a>
         <div class="row" id="home-page-our-etfs">
-            <div class="col-lg-2 offset-lg-1 col-sm-3 offset-sm-4" style="text-align: center">
-                <img src="<?= THEME_IMAGES_URI; ?>/Event%20Shares%20Icon-01-01.svg" id="home-firm-icon">
+            <div class="col-lg-2 offset-lg-1 col-sm-3 offset-sm-4 svg-image" style="text-align: center">
+                <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                     viewBox="-578 384.3 141.6 233.7" style="enable-background:new -578 384.3 141.6 233.7;"
+                     xml:space="preserve">
+                    <path class="st0" d="M-437.5,602.4l-54.2-102.9l53.9-102.4c3.2-6-2-12.8-9.6-12.8H-567c-7.7,0-12.8,6.8-9.6,12.8l53.9,101.6
+                        l-54.2,103.8c-3.8,7.3,2.4,15.6,11.6,15.6h116.1C-439.9,618-433.7,609.7-437.5,602.4z M-547.7,593.3l40.3-77.2l40.7,77.2H-547.7z"></path>
+                    </svg>
             </div>
             <div class="col-lg-4" id="left-column">
-                <h4><?php echo $pageContent['home_our_etfs_header']; ?></h4>
-                <p><?php echo $pageContent['home_our_etfs_paragraph']; ?></p>
+                <h4><?= $title ?></h4>
+				<?= $description ?>
             </div>
             <div class="col-lg-4">
                 <table class="table">
                     <tbody>
-                    <tr>
-                        <th>ABXD</th>
-                        <td>EVENTSHARES EMERGING ETFS</td>
-                    </tr>
-                    <tr>
-                        <th>ADCX</th>
-                        <td>EVENTSHARES EMERGING MARKETING</td>
-                    </tr>
-                    <tr>
-                        <th>GSDR</th>
-                        <td>EVENTSHARES EQULITY MARKETS</td>
-                    </tr>
-                    <tr>
-                        <th>SFRW</th>
-                        <td>EVENTSHARES EMERGING MARKETS</td>
-                    </tr>
-                    <tr>
-                        <th>DDFT</th>
-                        <td>EVENTSHARES EQULITY MARKETS</td>
-                    </tr>
-                    <tr>
-                        <th>JHGT</th>
-                        <td>EVENTSHARES SHORT MARKETS</td>
-                    </tr>
+					<?php
+					foreach ( $this->getRepeater( 'rows' ) as $index => $repeater ) : ?>
+                        <tr>
+                            <th><?= $repeater->getInput( 'rowBoldText' ); ?></th>
+                            <td><?= $repeater->getInput( 'rowText' ); ?></td>
+                        </tr>
+
+						<?php
+					endforeach;
+					?>
+
                     </tbody>
                 </table>
             </div>
