5ec6f3b78d2098183d0602a1f3a30319df204967
Piotr <pgurdek@gmail.com>
2017-06-06 10:57:18 +0200

    EV-16 code update

--- wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/class-module.php
+++ wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/class-module.php
@@ -7,41 +7,201 @@
 namespace Nurture\EventShares\Module;
 
 use Nurture\Pagebox\Module\AbstractModule;
+use Nurture\Pagebox\Module\Fields\Builder\Select;
 use Nurture\Pagebox\Module\View\StaticCacheInterface;
 
 class HomeBanner extends AbstractModule implements StaticCacheInterface {
 
-    /**
-     * Module config
-     * @return array Module configuration.
-     */
-    protected function config()
-    {
-        return [
-        	'version'       => '1.0.0',
-            'title'         => 'Home Banner With CTA',
-            'description'   => 'Home Banner'
-        ];
-    }
+	/**
+	 * Module config
+	 * @return array Module configuration.
+	 */
+	protected function config() {
+		return [
+			'version'     => '1.0.0',
+			'title'       => 'Home Banner With CTA',
+			'description' => 'Home Banner'
+		];
+	}
 
-    /**
-     * @return array Fields configuration.
-     */
-    protected function fields()
-    {
-        return [
-	        //Title
-	        'title' => [
-		        'type' => 'input:text',
-		        'label' => 'Title',
-		        'description' => 'Please enter title'
-	        ],
-	        'titleColor' => [
-		        'type' => 'input:color',
-		        'label' => 'Title color',
-		        'default' => '#282780',
-		        'sass' => true
-	        ],
-        ];
-    }
+	/**
+	 * @return array Fields configuration.
+	 */
+	protected function fields() {
+		return [
+			//Title
+			'title'           => [
+				'type'        => 'input:text',
+				'label'       => 'Title',
+				'description' => 'Please enter title'
+			],
+			'titleColor'      => [
+				'type'    => 'input:color',
+				'label'   => 'Title color',
+				'default' => '#fffff',
+				'sass'    => true
+			],
+			'titleInner'      => [
+				'type'        => 'input:text',
+				'label'       => 'Title colorful',
+				'description' => 'Please enter fancy title'
+			],
+			'titleInnerColor' => [
+				'type'        => 'input:color',
+				'label'       => 'Title colorful color',
+				'description' => 'Please enter fancy title color',
+				'default'     => '#55C2A2',
+				'sass'        => true
+			],
+			'titleSize'       => [
+				'type'        => 'input:text',
+				'label'       => 'Title size',
+				'description' => 'Please enter title size (px)',
+				'default'     => '54px',
+				'sass'        => true
+			],
+			'description'     => [
+				'type'        => 'editor',
+				'label'       => 'Description',
+				'description' => 'Please enter description',
+				'default'     => ''
+			],
+			'descriptionColor' => [
+				'type'        => 'input:color',
+				'label'       => 'Description color',
+				'description' => 'Please enter fancy description color',
+				'default'     => '#ffffff',
+				'sass'        => true
+			],
+			'descriptionSize'       => [
+				'type'        => 'input:text',
+				'label'       => 'Description size',
+				'description' => 'Please enter description size (px)',
+				'default'     => '14px',
+				'sass'        => true
+			],
+			'backgroundColor' => [
+				'type'        => 'input:color',
+				'label'       => 'Background color',
+				'description' => 'Please enter color',
+				'default'     => '#002841',
+				'sass'        => true
+			],
+			// Image
+			'bgImage'         => [
+				'type'        => 'media:image',
+				'label'       => 'Set background image',
+				'multiple'    => false,
+				'unique'      => false,
+				'aspectRatio' => '16:9',
+			],
+//	        Buttons
+
+			'buttonLeftTitle'     => [
+				'type'    => 'input:text',
+				'label'   => 'Button Left title',
+				'default' => ""
+			],
+			'buttonUrlLeft'       => [
+				'type'     => 'select',
+				'label'    => 'Page select',
+				'multiple' => false,
+				'options'  => [
+					'allowClear' => true
+				],
+				'values'   => function () {
+					return Select::postFilter( get_pages( [ 'posts_per_page' => - 1 ] ), [
+						'postID'    => function ( \WP_Post $post ) {
+							return $post->ID;
+						},
+						'permalink' => function ( \WP_Post $post ) {
+							return get_permalink( $post->ID );
+						}
+					] );
+				}
+			],
+			'buttonLeftBlankLink' => [
+				'type'    => 'input:switch',
+				'label'   => 'Link new target:',
+				'default' => 1,
+			],
+			'buttonLeftOn'        => [
+				'type'    => 'input:switch',
+				'label'   => 'Display',
+				'default' => 1,
+			],
+
+//	        Left Button End
+
+			'buttonMiddleTitle'     => [
+				'type'    => 'input:text',
+				'label'   => 'Button Middle title',
+				'default' => ""
+			],
+			'buttonUrlMiddle'       => [
+				'type'     => 'select',
+				'label'    => 'Page select',
+				'multiple' => false,
+				'options'  => [
+					'allowClear' => true
+				],
+				'values'   => function () {
+					return Select::postFilter( get_pages( [ 'posts_per_page' => - 1 ] ), [
+						'postID'    => function ( \WP_Post $post ) {
+							return $post->ID;
+						},
+						'permalink' => function ( \WP_Post $post ) {
+							return get_permalink( $post->ID );
+						}
+
+					] );
+				}
+			],
+			'buttonMiddleBlankLink' => [
+				'type'    => 'input:switch',
+				'label'   => 'Link new target:',
+				'default' => 1,
+			],
+			'buttonMiddleOn'        => [
+				'type'    => 'input:switch',
+				'label'   => 'Display',
+				'default' => 0,
+			],
+
+//			Next button
+			'buttonRightTitle'      => [
+				'type'    => 'input:text',
+				'label'   => 'Button Right title',
+				'default' => ""
+			],
+			'buttonUrlRight'        => [
+				'type'     => 'select',
+				'label'    => 'Page select',
+				'multiple' => false,
+				'options'  => [
+					'allowClear' => true
+				],
+				'values'   => function () {
+					return Select::postFilter( get_pages( [ 'posts_per_page' => - 1 ] ), [
+						'postID'    => function ( \WP_Post $post ) {
+							return $post->ID;
+						},
+						'permalink' => function ( \WP_Post $post ) {
+							return get_permalink( $post->ID );
+						}
+					] );
+				}
+			],
+			'buttonRightBlankLink'  => [
+				'type'    => 'input:switch',
+				'label'   => 'Link new target:',
+				'default' => 1,
+			],
+			'buttonRightOn'         => [
+				'type'    => 'input:switch',
+				'label'   => 'Display',
+				'default' => 0,
+			],
+		];
+	}
 }
--- wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/views/default.php
@@ -6,24 +6,71 @@
  * @var \Nurture\EventShares\Module\HomeBanner $module
  */
 
-$module = $this->getModule();
+$module       = $this->getModule();
+$bgImage      = $this->getMedia( 'bgImage' )->getImage()->getUrl( 'normal' );
+$title        = $this->getInput( 'title' );
+$titleInner   = $this->getInput( 'titleInner' );
+$description  = $this->getEditor( 'description' )->getContent();
+$buttonLeft   = $this->getInput( 'buttonLeftTitle' );
+$buttonMiddle = $this->getInput( 'buttonMiddleTitle' );
+$buttonRight  = $this->getInput( 'buttonRightTitle' );
 ?>
 <div class="<?= $module->getClass() ?>">
     <div class="container">
+
         <a class="task-number" target="_blank" href="https://nurture.atlassian.net/browse/EV-16">EV-16</a>
         <div class="row" id="home-banner">
+            <img src="<?= $bgImage ?>">
+
             <div class="col-lg-6">
-                <h2>active <br>weighting <span id="header-second-color">etf</span></h2>
+                <h2><?= $title ?><span id="header-second-color"><?= $titleInner ?></span></h2>
             </div>
             <div class="col-lg-12"></div>
             <div class="col-lg-5">
-                <p>Lorem ipsum dolor mollis pellentesque sit amet, consectetur adipiscing elit. Aenean mollis
-                    pellentesque sodales.
-                    Vestibulum vel nunc consectetur adipiscing elit sed nisi dapibus dictum. Curabitur aliquet tortor
-                    vitae pellentesque ultricies.</p>
-                <a href="#" class="btn btn-primary" id="learn-more-button">
-                    <span class="align-middle">learn more</span>
-                </a>
+				<?= $description ?>
+				<?php
+//                Left button
+				if ( $this->getInput( 'buttonLeftOn' )->getValue() )
+					:?>
+					<?php
+					$url   = $this->getSelect( 'buttonUrlLeft' )->getValue()['permalink'];
+					$blank = $this->getInput( 'buttonLeftBlankLink' )->getValue() ? 'target=_blank' : "";
+					?>
+                    <a href="<?= $url ?>" <?= $blank ?> class="btn btn-primary learn-more-button">
+                        <span class="align-middle"><?= $buttonLeft ?></span>
+                    </a>
+					<?php
+				endif;
+				?>
+
+	            <?php
+//                Middle Button
+	            if ( $this->getInput( 'buttonMiddleOn' )->getValue() )
+		            :?>
+		            <?php
+		            $url   = $this->getSelect( 'buttonUrlMiddle' )->getValue()['permalink'];
+		            $blank = $this->getInput( 'buttonMiddleBlankLink' )->getValue() ? 'target=_blank' : "";
+		            ?>
+                    <a href="<?= $url ?>" <?= $blank ?> class="btn btn-primary learn-more-button">
+                        <span class="align-middle"><?= $buttonMiddle ?></span>
+                    </a>
+		            <?php
+	            endif;
+	            ?>
+	            <?php
+//                Right Button
+	            if ( $this->getInput( 'buttonRightOn' )->getValue() )
+		            :?>
+		            <?php
+		            $url   = $this->getSelect( 'buttonUrlRight' )->getValue()['permalink'];
+		            $blank = $this->getInput( 'buttonRightBlankLink' )->getValue() ? 'target=_blank' : "";
+		            ?>
+                    <a href="<?= $url ?>" <?= $blank ?> class="btn btn-primary learn-more-button">
+                        <span class="align-middle"><?= $buttonRight ?></span>
+                    </a>
+		            <?php
+	            endif;
+	            ?>
             </div>
         </div>
     </div>
