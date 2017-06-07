af263c0dca0550bd5ee78df53eac33f4dc609838
Piotr <pgurdek@gmail.com>
2017-06-07 13:56:42 +0200

    EV-50 code update

--- wp-content/themes/event-shares/pagebox/modules/get_in_touch/class-module.php
+++ wp-content/themes/event-shares/pagebox/modules/get_in_touch/class-module.php
@@ -7,6 +7,7 @@
 namespace Nurture\EventShares\Module;
 
 use Nurture\Pagebox\Module\AbstractModule;
+use Nurture\Pagebox\Module\Fields\Builder\Select;
 use Nurture\Pagebox\Module\View\StaticCacheInterface;
 
 class GetInTouch extends AbstractModule implements StaticCacheInterface {
@@ -33,13 +34,49 @@ class GetInTouch extends AbstractModule implements StaticCacheInterface {
 	        //Title
 	        'title' => [
 		        'type' => 'input:text',
-		        'label' => 'Title',
+		        'label' => 'Enter Text',
 		        'description' => 'Please enter title'
 	        ],
 	        'titleColor' => [
 		        'type' => 'input:color',
 		        'label' => 'Title color',
-		        'default' => '#282780',
+		        'default' => '#ffffff',
+		        'sass' => true
+	        ],
+	        'titleSize' => [
+		        'type' => 'input:text',
+		        'label' => 'Text size',
+		        'default' => '30px',
+		        'sass' => true
+	        ],
+	        'buttonUrl'       => [
+		        'type'     => 'select',
+		        'label'    => 'Choose page to redirect',
+		        'multiple' => false,
+		        'options'  => [
+			        'allowClear' => true
+		        ],
+		        'values'   => function () {
+			        return Select::postFilter( get_pages( [ 'posts_per_page' => - 1 ] ), [
+				        'postID'    => function ( \WP_Post $post ) {
+					        return $post->ID;
+				        },
+				        'permalink' => function ( \WP_Post $post ) {
+					        return get_permalink( $post->ID );
+				        }
+			        ] );
+		        }
+	        ],
+	        'buttonBlankLink' => [
+		        'type'    => 'input:switch',
+		        'label'   => 'Link new target:',
+		        'default' => 1,
+	        ],
+
+	        'backgroundColor' => [
+		        'type' => 'input:color',
+		        'label' => 'Background color',
+		        'default' => '#002841',
 		        'sass' => true
 	        ],
         ];
--- wp-content/themes/event-shares/pagebox/modules/get_in_touch/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/get_in_touch/views/default.php
@@ -12,7 +12,18 @@ $module = $this->getModule();
     <div class="container">
         <a class="task-number" target="_blank" href="https://nurture.atlassian.net/browse/EV-50">EV-50</a>
         <div class="row">
-            <div class="col-lg-12" id="get-in-touch">GET IN TOUCH</div>
+            <div class="col-lg-12 get-in-touch">
+	            <?php
+	            $url   = $this->getSelect( 'buttonUrl' )->getValue()['permalink'];
+	            if (empty($url) && !isset($url)){
+	                $url = get_permalink(23);
+                }
+	            $blank = $this->getInput( 'buttonBlankLink' )->getValue() ? 'target=_blank' : "";
+	            ?>
+                <a href="<?=$url?>" <?=$blank?>>
+                <?=$this->getInput('title')?>
+                </a>
+            </div>
         </div>
     </div>
 </div>
