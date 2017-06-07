f2a3f580f5caa611ad49ec81b87dce79fee0149b
Piotr <pgurdek@gmail.com>
2017-06-07 08:35:32 +0200

    EV-19 - code update -> add styles for module

--- wp-content/themes/event-shares/pagebox/modules/icons_and_text/class-module.php
+++ wp-content/themes/event-shares/pagebox/modules/icons_and_text/class-module.php
@@ -11,60 +11,117 @@ use Nurture\Pagebox\Module\View\StaticCacheInterface;
 
 class IconsAndText extends AbstractModule implements StaticCacheInterface {
 
-    /**
-     * Module config
-     * @return array Module configuration.
-     */
-    protected function config()
-    {
-        return [
-        	'version'       => '1.0.0',
-            'title'         => 'Icons and text',
-            'description'   => 'Three columns with image and text'
-        ];
-    }
+	/**
+	 * Module config
+	 * @return array Module configuration.
+	 */
+	protected function config() {
+		return [
+			'version'     => '1.0.0',
+			'title'       => 'Icons and text',
+			'description' => 'Three columns with image and text'
+		];
+	}
 
-    /**
-     * @return array Fields configuration.
-     */
-    protected function fields()
-    {
-        return [
-	        //Title
-	        'title' => [
-		        'type' => 'input:text',
-		        'label' => 'Title',
-		        'description' => 'Please enter title'
-	        ],
-	        'titleColor' => [
-		        'type' => 'input:color',
-		        'label' => 'Title color',
-		        'default' => '#002842',
-		        'sass' => true
-	        ],
-//	        First Section
-	        'firstSectionSwitch'       => [
-		        'type'    => 'input:switch',
-		        'label'   => 'SVG(Marker) / Image:',
-		        'default' => 0,
-	        ],
-	        'firstSectionIconColor' => [
-		        'type' => 'input:color',
-		        'label' => 'Title color',
-		        'default' => '#002842',
-		        'sass' => true
-	        ],
-	        'firstSectionImage'          => [
-		        'type'        => 'media:image',
-		        'label'       => 'Set background image First Section',
-		        'multiple'    => false,
-		        'unique'      => false,
-		        'aspectRatio' => '16:9',
-	        ],
-	        'firstSectionEditor' =>[
-	        	'type'=>'editor',
-		        'label' =>'Set Text for first Section'
-	        ]
-        ];
-    }
+	/**
+	 * @return array Fields configuration.
+	 */
+	protected function fields() {
+		return [
+			//Title
+			'title'            => [
+				'type'        => 'input:text',
+				'label'       => 'Title',
+				'description' => 'Please enter title'
+			],
+			'titleColor'       => [
+				'type'    => 'input:color',
+				'label'   => 'Title color',
+				'default' => '#002842',
+				'sass'    => true
+			],
+			'titleSize'        => [
+				'type'        => 'input:text',
+				'label'       => 'Title size',
+				'description' => 'Please enter title size (px)',
+				'default'     => '25px',
+				'sass'    => true
+			],
+//	        Repeater
+			'sections'         => [
+				'type'   => 'repeater',
+				'label'  => 'Sections',
+				'fields' => [
+
+					'sectionImage'       => [
+						'type'        => 'media:image',
+						'label'       => 'Set background image First Section',
+						'multiple'    => false,
+						'unique'      => false,
+						'aspectRatio' => '16:9',
+					],
+					'sectionEditor'      => [
+						'type'  => 'editor',
+						'label' => 'Set Text for section'
+					],
+					'sectionButton'      => [
+						'type'    => 'input:text',
+						'label'   => 'Button Text:',
+						'default' => '',
+					],
+					'sectionButtonUrl'   => [
+						'type'    => 'input:text',
+						'label'   => 'Button Url Text:',
+						'default' => '#',
+					],
+					'sectionButtonBlank' => [
+						'type'    => 'input:switch',
+						'label'   => 'Target Blank:',
+						'default' => 1,
+					],
+				],
+			],
+//			End Repeater
+			'titleInnerSize'   => [
+				'type'        => 'input:text',
+				'label'       => 'Title Inner size',
+				'description' => 'Please enter title size (px)',
+				'default'     => '25px',
+				'sass'    => true
+			],
+			'titleInnerColor'  => [
+				'type'    => 'input:color',
+				'label'   => 'Title color',
+				'default' => '#002842',
+				'sass'    => true
+			],
+
+			'descriptionColor' => [
+				'type'    => 'input:color',
+				'label'   => 'Description color',
+				'default' => '#292b2c',
+				'sass'    => true
+			],
+			'descriptionSize'  => [
+				'type'        => 'input:text',
+				'label'       => 'Description size',
+				'description' => 'Please enter title size (px)',
+				'default'     => '14px',
+				'sass'    => true
+			],
+			'sectionIconColor'      => [
+				'type'    => 'input:color',
+				'label'   => 'Icon (if exist) color',
+				'default' => '#56C1A3',
+				'sass'    => true
+			],
+			'sectionIconColorHover' => [
+				'type'    => 'input:color',
+				'label'   => 'Icon color on hover',
+				'default' => '#002841',
+				'sass'    => true
+			],
+
+		];
+	}
 }
--- wp-content/themes/event-shares/pagebox/modules/icons_and_text/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/icons_and_text/views/default.php
@@ -7,27 +7,59 @@
  */
 
 $module = $this->getModule();
+
 ?>
 <div class="<?= $module->getClass() ?>">
-    <div class="container"><?php include_once('pageContent.php') ?>
+    <div class="container"><?php include_once( 'pageContent.php' ) ?>
         <a class="task-number" target="_blank" href="https://nurture.atlassian.net/browse/EV-19">EV-19</a>
         <div class="row" id="three-column-boxes">
-            <div class="col-lg-12"><h4 id="top-header"><?php echo $pageContent['header2']; ?></h4></div>
-            <div class="col-lg-4">
-                <div style="margin-bottom: 20px; height: 105px"><img src="<?= THEME_IMAGES_URI; ?>/ES Nav Icon-01-01.svg" id="icon1"></div>
-                <h4 class="sub-header"><?php echo $pageContent['three_column_sub_header1']; ?></h4>
-                <p><?php echo $pageContent['three_column_sub_paragraph1']; ?></p>
-            </div>
-            <div class="col-lg-4">
-                <div style="margin-bottom: 20px;  height: 105px"><img src="<?= THEME_IMAGES_URI; ?>/ES%20Magnifyglass-Graph%20Icon-01-01.svg" id="icon2"></div>
-                <h4 class="sub-header"><?php echo $pageContent['three_column_sub_header2']; ?></h4>
-                <p><?php echo $pageContent['three_column_sub_paragraph2']; ?></p>
-            </div>
-            <div class="col-lg-4">
-                <div style="margin-bottom: 20px;  height: 105px"><img src="<?= THEME_IMAGES_URI; ?>/ES%20Pie%20Graph%20Icon-01-01.svg" id="icon3"></div>
-                <h4 class="sub-header"><?php echo $pageContent['three_column_sub_header3']; ?></h4>
-                <p><?php echo $pageContent['three_column_sub_paragraph3']; ?></p>
-            </div>
+
+
+            <div class="col-lg-12"><h2 id="top-header"><?= $this->getInput( 'title' ) ?></h2></div>
+			<?php foreach ( $this->getRepeater( 'sections' ) as $index => $section ) :
+
+				$sectionEditor = $section->getEditor( 'sectionEditor' )->getValue();
+				$sectionImageID = $section->getMedia( 'sectionImage' )->getImage()->getId();
+				?>
+                <div class="col-lg-4">
+                    <div class="svg-wrapper first-svg"> <?php echo wp_get_attachment_image( $sectionImageID, 'full', false, [ 'class' => 'style-svg' ] ) ?></div>
+                    <p><?= $sectionEditor ?></p>
+
+					<?php
+
+					$buttonText = $section->getInput( 'sectionButton' );
+					$url        = $section->getInput( 'sectionButtonUrl' );
+					$blank      = $section->getInput( 'sectionButtonBlank' )->getValue() ? 'target=_blank' : "";
+//
+					if ( ! empty( $buttonText ) && isset( $buttonText ) ) :
+						?>
+                        <a href="<?= $url ?>" class="button " <?= $blank ?> ><?= $buttonText ?></a>
+
+						<?php
+					endif;
+					?>
+                </div>
+
+				<?php
+			endforeach;
+			?>
+
+            <!--            <div class="col-lg-4">-->
+            <!--                <div style="margin-bottom: 20px;  height: 105px"><img-->
+            <!--                            src="-->
+			<? //= THEME_IMAGES_URI; ?><!--/ES%20Magnifyglass-Graph%20Icon-01-01.svg" id="icon2"></div>-->
+            <!--                <h4 class="sub-header">-->
+			<?php //echo $pageContent['three_column_sub_header2']; ?><!--</h4>-->
+            <!--                <p>--><?php //echo $pageContent['three_column_sub_paragraph2']; ?><!--</p>-->
+            <!--            </div>-->
+            <!--            <div class="col-lg-4">-->
+            <!--                <div style="margin-bottom: 20px;  height: 105px"><img-->
+            <!--                            src="-->
+			<? //= THEME_IMAGES_URI; ?><!--/ES%20Pie%20Graph%20Icon-01-01.svg" id="icon3"></div>-->
+            <!--                <h4 class="sub-header">-->
+			<?php //echo $pageContent['three_column_sub_header3']; ?><!--</h4>-->
+            <!--                <p>--><?php //echo $pageContent['three_column_sub_paragraph3']; ?><!--</p>-->
+            <!--            </div>-->
         </div>
     </div>
 </div>
