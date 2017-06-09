74622b2f841b68589f4c5e0c0a6a34eba1c157a9
Piotr <pgurdek@gmail.com>
2017-06-09 08:12:09 +0200

    EV-15 - add js functionality to detected mobile/desktop , add sticky detection

--- wp-content/themes/event-shares/assets/javascripts/script.js
+++ wp-content/themes/event-shares/assets/javascripts/script.js
@@ -6,5 +6,83 @@ jQuery(function ($) {
      */
     //$window = $( window );
     //$htmlBody = $( "html, body" );
+    var $window = $(window);
+    var $document = $(document);
+    var $body = $("body");
+    /**
+     * Add/remove class on scroll
+     **/
+    var $wpgMainHeader = $("#wpx-eventshare-header");
+
+    var sizeBar = function () {
+        if ($window.scrollTop() > 400) {
+            $wpgMainHeader.addClass("wpx-small-header");
+        } else {
+            $wpgMainHeader.removeClass("wpx-small-header");
+        }
+    };
+
+    var mobileVer = function () {
+        if ($(".wpx-button-hamburger").css("display") === "block") {
+            $body.addClass("wpg-mobile").removeClass("wpx-desktop");
+        } else {
+            $body.removeClass("wpg-mobile").addClass("wpx-desktop");
+        }
+    };
+
+    mobileVer();
+
+    $window.on("resize", mobileVer);
+    $window.on("scroll", sizeBar);
+
+
+
+    /* mobile hamburger */
+    (function ($) {
+
+
+        var sizeBar = function () {
+            if ($window.scrollTop() > 400) {
+                $wpgMainHeader.addClass("wpx-small-header");
+            } else {
+                $wpgMainHeader.removeClass("wpx-small-header");
+            }
+        };
+
+
+        $(".wpx-button-hamburger").on("click", function (event) {
+            event.preventDefault();
+            $wpgMainHeader
+                .toggleClass("wpx-active-mobile-menu-parent")
+                .find("nav")
+                .toggleClass("wpx-active-mobile-menu");
+
+        });
+
+        $window.on("resize", mobileVer);
+        $window.on("load", mobileVer);
+        $window.on("scroll", sizeBar);
+        //close mobile menu
+
+        // $document.on("click", "body", function (e) {
+        //     if ($wpgMainHeader.hasClass("wpx-active-mobile-menu-parent")) {
+        //         if ($(e.target).is('.wpx-main-header *')) {
+        //             return;
+        //         } else {
+        //             $(".wpx-button-hamburger").trigger("click");
+        //         }
+        //     }
+        // });
+        // $body.on("swiperight", function () {
+        //     if ($wpgMainHeader.hasClass("wpx-active-mobile-menu-parent")) {
+        //         $(".wpx-button-hamburger").trigger("click");
+        //
+        //     }
+        // });
+
+
+    })(jQuery);
+
+
 
 });
