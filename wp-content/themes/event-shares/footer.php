72b5b5416a4545665c6c63a5522340399a517e0f
Piotr <pgurdek@gmail.com>
2017-06-12 20:36:02 +0200

    EV-21 - removed static data, added dynamic menu, custom styles from menu, dynamic data

--- wp-content/themes/event-shares/footer.php
+++ wp-content/themes/event-shares/footer.php
@@ -1,157 +1,94 @@
 <footer>
-    <?php include_once('pageContent.php');
-    $pageContent = array(
-        //footer
-        'get_in_touch' => 'GET IN TOUCH'
-    , 'footer_left_text' => 'Lorem ipsum dolor sit amet, duis vitae et ullamcorper, vitae quam, netus neque morbi mauris sed vel tempor.'
-    , 'footer_right_text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at. Nam varius eleifend risus ac aliquam. Sed semper turpis enim, non dignissim lorem convallis vitae. Duis quis sapien arcu. Phasellus vitae massa arcu.'
-    , 'copyright' => 'COPYRIGHT 2017. ALL RIGHTS RESERVED'
-    , 'footer_menu_item1' => 'ABOUT US'
-    , 'footer_menu_item2' => 'FUNDS IN FOCUS'
-    , 'footer_menu_item3' => 'NEWS & INSIGHTS'
-    , 'footer_menu_item4' => 'CONTACT US'
-    , 'footer_menu_item5' => 'TERMS'
-        //top menu
-    , 'tom_menu_item1' => 'ABOUT US'
-    , 'tom_menu_item2' => 'OUR ETFS'
-    , 'tom_menu_item3' => 'NEWS & INSIGHTS'
-    , 'tom_menu_item4' => 'CONTACT US'
-    , 'about-us-sub1' => 'Our Firm'
-    , 'about-us-sub2' => 'Our Team'
-    , 'about-us-sub3' => 'ETF Education'
-    , 'learn-more' => 'LEARN MORE'
-    , 'lorem' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis pellentesque sodales. Vestibulum vel nunc sed nisi dapibus dictum. Curabitur aliquet tortor vitae pellentesque ultricies. Ut faucibus sapien dolor, eget interdum leo finibus id.'
-    , 'lorem_short' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis pellentesque sodales. Vestibulum vel nunc sed nisi dapibus dictum.'
-        // banner with download button
-    , 'header1' => 'LOREM IPSUM'
-    , 'paragraph1' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed leo nec nulla lobortis dignissim
-                   sit amet at lorem. Morbi aliquam id velit id gravida. In massa nunc, pellentesque vitae ligula at, condimentum 
-                    hendrerit ante.'
-    , 'download_button' => 'DOWNLOAD PDF'
-        // three column boxes
-    , 'header2' => 'our investment philosophy'
-    , 'three_column_sub_header1' => 'lorem ipsum'
-    , 'three_column_sub_header2' => 'lorem ipsum'
-    , 'three_column_sub_header3' => 'lorem ipsum'
-    , 'three_column_sub_paragraph1' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis pellentesque sodales. Vestibulum vel nunc sed nisi dapibus dictum. Curabitur aliquet tortor vitae pellentesque ultricies.'
-    , 'three_column_sub_paragraph2' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis pellentesque sodales. Vestibulum vel nunc sed nisi dapibus dictum. Curabitur aliquet tortor vitae pellentesque ultricies.'
-    , 'three_column_sub_paragraph3' => 'lorem ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis pellentesque sodales. Vestibulum vel nunc sed nisi dapibus dictum. Curabitur aliquet tortor vitae pellentesque ultricies.'
-        // our history
-    , 'our_history_header' => 'our history'
-    , 'our_history_content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at.'
-        // in the media
-    , 'in_the_media_header' => 'in the media'
-        //firm partnership
-    , 'firm_partnership' => 'firm partnership'
-    , 'firm_partnership_content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at. Nam varius eleifend risus ac aliquam.'
-        //board of advisors
-    , 'board_of_advisors' => 'board of advisors'
-    , 'board_of_advisors_paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at. Nam varius eleifend risus ac aliquam.'
-    , 'header11' => 'Joe Bloggs'
-    , 'paragraph11' => 'Senior Advisor'
-    , 'header12' => 'Joseph Baratta'
-    , 'paragraph12' => 'Senior Advisor'
-    , 'header13' => 'David Colhoun'
-    , 'paragraph13' => 'Senior Advisor'
-    , 'header14' => 'David S. Blitzer'
-    , 'paragraph14' => 'Senior Advisor'
+	<?= createTaskLink( 'EV-21' ) ?>
+    <style>
 
-    , 'header21' => 'Joe Bloggs'
-    , 'paragraph21' => 'Senior Advisor'
-    , 'header22' => 'Joseph Baratta'
-    , 'paragraph22' => 'Senior Advisor'
-    , 'header23' => 'David Colhoun'
-    , 'paragraph23' => 'Senior Advisor'
-    , 'header24' => 'David S. Blitzer'
-    , 'paragraph24' => 'Senior Advisor'
+        /*  CUSTOM STYLES FROM WPX_THEME_OPTIONS */
 
-    , 'header31' => 'Joe Bloggs'
-    , 'paragraph31' => 'Senior Advisor'
-    , 'header32' => 'Joseph Baratta'
-    , 'paragraph32' => 'Senior Advisor'
-    , 'header33' => 'David Colhoun'
-    , 'paragraph33' => 'Senior Advisor'
-    , 'header34' => 'David S. Blitzer'
-    , 'paragraph34' => 'Senior Advisor'
+        /*  Footer left/right editor   */
 
-        // article-tile
-    , 'article_date' => '21.10.2016'
-    , 'article_author' => 'Joe Bloggs'
-    , 'article_title' => 'consectetur adipis aliquam commodo'
-    , 'article_paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien.'
-        // contacts
-    , 'contacts' => 'contacts'
-    , 'contact_header1' => 'Investors'
-    , 'contact_paragraph11' => '1-1111-GG-FUND1'
-    , 'contact_paragraph12' => '1-8888-123-2345'
-    , 'contact_paragraph13' => 'info@eventshares.com'
+        footer #footer #left-column {
+            font-size: <?=wpx_theme_get_option('wpx_theme_footer_small_text_size')?>;
+            color: <?=wpx_theme_get_option('wpx_theme_footer_small_text_color')?>;
+        }
 
-    , 'contact_header2' => 'Investors'
-    , 'contact_paragraph21' => '1-1111-GG-FUND1'
-    , 'contact_paragraph22' => '1-8888-123-2345'
-    , 'contact_paragraph23' => 'info@eventshares.com'
+        footer #footer #right-column {
+            font-size: <?=wpx_theme_get_option('wpx_theme_footer_big_text_size')?>;
+            color: <?=wpx_theme_get_option('wpx_theme_footer_big_text_color')?>;
+        }
 
-    , 'contact_header3' => 'Investors'
-    , 'contact_paragraph31' => '1-1111-GG-FUND1'
-    , 'contact_paragraph32' => '1-8888-123-2345'
-    , 'contact_paragraph33' => 'info@eventshares.com'
+        /*End Footer left/right enditor*/
 
-    , 'contact_header4' => 'Investors'
-    , 'contact_paragraph41' => '1-1111-GG-FUND1'
-    , 'contact_paragraph42' => '1-8888-123-2345'
-    , 'contact_paragraph43' => 'info@eventshares.com'
+        /*  Footer menu links   */
+        footer #menu-items ul li a {
+            -webkit-transition: 300ms all;
+            -moz-transition: 300ms all;
+            -ms-transition: 300ms all;
+            -o-transition: 300ms all;
+            transition: 300ms all;
+            color: <?=wpx_theme_get_option('wpx_theme_footer_link_color')?>;
+        }
 
-        // contact form
-    , 'send_button' => 'send'
-    , 'contact_form_paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at. Nam varius eleifend risus ac aliquam.'
+        footer #menu-items ul li a:hover {
+            color: <?=wpx_theme_get_option('wpx_theme_footer_link_color_hover')?>;
+        }
 
-        // top page image with text
-    , 'paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at.'
+        /*  End Footer menu links   */
 
-        // home page our etfs
-    , 'home_our_etfs_paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien, vel efficitur dui vehicula laoreet. Sed facilisis ornare tellus a mattis. Mauris volutpat enim eget nibh finibus, quis tempor orci pretium. Ut gravida vehicula dui, ac tincidunt arcu feugiat at.'
-    , 'home_our_etfs_header' => 'our etfs'
-        // search results
-    , 'page_results' => 'page results'
-    , 'result_header' => 'lorem ipsum'
-    , 'result_paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sapien sapien. Sed facilisis ornare tellus a mattis. Ut gravida vehicula dui, ac tincidunt arcu feugiat at... '
-    , 'read_more' => 'read more'
-    );
-//    ?>
+        /* Footer icon custom color*/
+        footer #footer-nav svg path, footer #footer-nav svg rect {
+            fill: <?=wpx_theme_get_option('wpx_theme_footer_icon_color')?>;
+
+        }
+
+        footer #footer-nav svg:hover path, footer #footer-nav svg:hover rect {
+            fill: <?=wpx_theme_get_option('wpx_theme_footer_icon_color_hover')?>;
+
+        }
+
+        /*End Footer icon custom color*/
+
+        /*  Footer Copyrights styles   */
+        footer #copyright {
+            color: <?=wpx_theme_get_option('wpx_theme_footer_copyright_color')?>;
+            font-size: <?=wpx_theme_get_option('wpx_theme_footer_copyright_size')?>;
+
+        }
+
+        /*  End Footer Copyrights styles   */
+
+    </style>
     <div class="container">
         <div class="row" id="footer">
-            <div class="col-md-3 col-sm-10" id="left-column"><?php echo $pageContent['footer_left_text']; ?></div>
-            <div class="col-md-6 col-sm-10" id="right-column"><?php echo $pageContent['footer_right_text']; ?></div>
+            <div class="col-md-3 col-sm-10"
+                 id="left-column"><?php echo wpx_theme_get_option( 'wpx_theme_footer_small_text' ); ?></div>
+            <div class="col-md-6 col-sm-10"
+                 id="right-column"><?php echo wpx_theme_get_option( 'wpx_theme_footer_big_text' ) ?></div>
         </div>
         <div class="row" id="footer-nav">
             <div class="col-md-3 col-sm-4 col-6" id="social-wrapper">
-                <img src="<?= THEME_IMAGES_URI; ?>/Facebook%20Green%20Icon-01-01.svg">
-                <img src="<?= THEME_IMAGES_URI; ?>/Twitter%20Green%20Icon-01-01.svg">
-                <img src="<?= THEME_IMAGES_URI; ?>/Linkedin%20Green%20Icon-01-01.svg">
+                <img class="style-svg" src="<?= THEME_IMAGES_URI; ?>/Facebook%20Green%20Icon-01-01.svg">
+                <img class="style-svg" src="<?= THEME_IMAGES_URI; ?>/Twitter%20Green%20Icon-01-01.svg">
+                <img class="style-svg" src="<?= THEME_IMAGES_URI; ?>/Linkedin%20Green%20Icon-01-01.svg">
             </div>
             <div class="col-md-9 col-sm-8 col-6" id="menu-items">
-                <ul>
-                    <li id="about-us">
-                        <a href="#"><?php echo $pageContent['footer_menu_item1']; ?></a>
-                    </li>
-                    <li>
-                        <a href="#"><?php echo $pageContent['footer_menu_item2']; ?></a>
-                    </li>
-                    <li>
-                        <a href="#"><?php echo $pageContent['footer_menu_item3']; ?></a>
-                    </li>
-                    <li>
-                        <a href="#"><?php echo $pageContent['footer_menu_item4']; ?></a>
-                    </li>
-                    <li>
-                        <a href="#"><?php echo $pageContent['footer_menu_item5']; ?></a>
-                    </li>
-                </ul>
+				<?php
+				if ( has_nav_menu( 'footer' ) ) {
+					wp_nav_menu( array(
+							'theme_location' => 'footer',
+							'container'      => false,
+							'depth'          => 1
+						)
+					);
+				}
+
+				?>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-3" id="copyright"><?php echo $pageContent['copyright']; ?></div>
+            <div class="col-lg-3" id="copyright"><?=wpx_theme_get_option('wpx_theme_footer_copyright_text')?></div>
         </div>
     </div>
 </footer>
-<?php wp_footer(); ?></body></html>
+<?php wp_footer(); ?>
+</body>
+</html>
