4caf9d2dae2968519674edb6d4997bd6a03f6c4a
Piotr <pgurdek@gmail.com>
2017-06-15 10:59:31 +0200

    EV-29 - add basic flip to form and replace with dynamic data

--- wp-content/themes/event-shares/pagebox/modules/contact/class-module.php
+++ wp-content/themes/event-shares/pagebox/modules/contact/class-module.php
@@ -11,42 +11,71 @@ use Nurture\Pagebox\Module\View\StaticCacheInterface;
 
 class Contact extends AbstractModule implements StaticCacheInterface {
 
-    /**
-     * Module config
-     * @return array Module configuration.
-     */
-    protected function config()
-    {
-        return [
-        	'version'       => '1.0.0',
-            'title'         => 'Contact Form',
-            'description'   => 'Contact module'
-        ];
-    }
-    /**
-     * @return array Fields configuration.
-     */
-    protected function fields()
-    {
-        return [
-	        //Title
-	        'title' => [
-		        'type' => 'input:text',
-		        'label' => 'Title',
-		        'description' => 'Please enter title'
-	        ],
-	        'titleColor' => [
-		        'type' => 'input:color',
-		        'label' => 'Title color',
-		        'default' => '#282780',
-		        'sass' => true
-	        ],
-            'backgroundColor' => [
-                'type' => 'input:color',
-                'label' => 'Background color',
-                'default' => '#ffffff',
-                'sass' => true
-            ],
-        ];
-    }
+	/**
+	 * Module config
+	 * @return array Module configuration.
+	 */
+	protected function config() {
+		return [
+			'version'     => '1.0.0',
+			'title'       => 'Contact Form',
+			'description' => 'Contact module'
+		];
+	}
+
+	/**
+	 * @return array Fields configuration.
+	 */
+	protected function fields() {
+		return [
+			//Title
+			'title'           => [
+				'type'        => 'input:text',
+				'label'       => 'Title',
+				'description' => 'CONTACTS'
+			],
+			'backgroundColor' => [
+				'type'    => 'input:color',
+				'label'   => 'Background color',
+				'default' => '#ffffff',
+				'sass'    => true
+			],
+
+			'addresses' => [
+				'type'   => 'repeater',
+				'label'  => 'Addresses',
+				'fields' => [
+					'title'       => [
+						'type'        => 'input:text',
+						'label'       => 'Title',
+						'description' => 'Please enter title'
+					],
+					'isTitle'       => [
+						'type'        => 'input:switch',
+						'label'       => 'Title(off/on)',
+						'description' => 'Please pick option',
+						'default' => 1
+					],
+					'description' => [
+						'type'        => 'editor',
+						'label'       => 'Description',
+						'description' => 'Please enter description'
+					],
+				],
+
+			],
+
+			'contactFormShortCode' => [
+				'type'        => 'input:text',
+				'label'       => 'Contact Form 7 Shortcode',
+				'description' => ''
+			],
+
+			'subscriptionDescription' => [
+				'type'        => 'editor',
+				'label'       => 'Subscription Description',
+				'description' => 'Please enter description'
+			],
+		];
+	}
 }
--- wp-content/themes/event-shares/pagebox/modules/contact/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/contact/views/default.php
@@ -9,69 +9,40 @@
 $module = $this->getModule();
 ?>
 <div class="<?= $module->getClass() ?>">
-    <div class="container">
-        <?= createTaskLink('EV-29') ?>
-        <div class="row">
-            <?php include_once('pageContent.php');?>
-            <div class="flex-lg-row col-lg-6 col-sm-12" id="contacts">
-                <div class="row">
-                    <div class="col-lg-12" ><h4><?php echo $pageContent['contacts'] ?></h4></div>
-                    <div class="col-lg-4 col-sm-12 padding-bottom" >
-                        <h5><?php echo $pageContent['contact_header1'] ?></h5>
-                        <span><?php echo $pageContent['contact_paragraph11'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph12'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph13'] ?></span>
-                    </div>
-                    <div class="col-lg-4 col-sm-12 offset-lg-2 padding-bottom">
-                        <h5>Media Inquiries</h5>
-                        <span><?php echo $pageContent['contact_paragraph11'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph12'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph13'] ?></span>
-                    </div>
-                    <div class="col-lg-4 col-sm-12 padding-bottom">
-                        <h5><?php echo $pageContent['contact_header1'] ?></h5>
-                        <span><?php echo $pageContent['contact_paragraph11'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph12'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph13'] ?></span>
-                    </div>
-                    <div class="col-lg-4 offset-lg-2 col-sm-12 padding-bottom">
-                        <h5><?php echo $pageContent['contact_header1'] ?></h5>
-                        <span><?php echo $pageContent['contact_paragraph11'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph12'] ?><br></span>
-                        <span><?php echo $pageContent['contact_paragraph13'] ?></span>
+    <div class="container flip-container">
+		<?= createTaskLink( 'EV-29' ) ?>
+        <div class="flipper">
+            <div class="row front">
+				<?php include_once( 'pageContent.php' ); ?>
+                <div class="flex-lg-row col-lg-6 col-sm-12" id="contacts">
+                    <div class="row">
+                        <div class="col-lg-12"><h4><?= $this->getInput( 'title' ); ?></h4></div>
+						<?php foreach ( $this->getRepeater( 'addresses' ) as $index => $address ) : ?>
+                            <div class="col-lg-6 col-sm-12 padding-bottom">
+								<?php if ( $address->getInput( 'isTitle' )->getValue() ) : ?>
+                                    <div>
+                                        <h5><?= $address->getInput( 'title' ) ?></h5>
+                                    </div>
+								<?php else: ?>
+                                    <div style="padding-top: 26px"></div>
+								<?php endif; ?>
+
+								<?= $address->getEditor( 'description' )->getContent(); ?>
+                            </div>
+						<?php endforeach; ?>
+
                     </div>
                 </div>
-            </div>
+                <div id="contact-form" class="col-lg-6 col-sm-12">
 
-            <div id="contact-form" class="col-lg-6 col-sm-12">
-                <form class="row">
-                    <div class="form-group col-lg-6">
-                        <input type="text" class="form-control small-input rounded-0" id="first-name"
-                               placeholder="First name">
-                    </div>
-                    <div class="form-group col-lg-6">
-                        <input type="text" class="form-control small-input rounded-0" id="last-name"
-                               placeholder="Last name">
-                    </div>
-                    <div class="form-group col-lg-6">
-                        <input type="tel" class="form-control small-input rounded-0" id="tel-number"
-                               placeholder="Tel number">
-                    </div>
-                    <div class="form-group col-lg-6">
-                        <input type="email" class="form-control small-input rounded-0" id="email-address"
-                               placeholder="Email address">
-                    </div>
-                    <div class="form-group col-lg-12">
-                        <textarea type="text" class="form-control rounded-0" id="message" placeholder="Message"></textarea>
-                    </div>
-                    <div class="form-group col-lg-12">
-                        <button type="submit" class="btn btn-primary" id="submit-button">
-                            <?php echo $pageContent['send_button']; ?>
-                        </button>
-                        <p><?php echo $pageContent['contact_form_paragraph']; ?></p>
-                    </div>
-                </form>
+					<?php if ( shortcode_exists( 'contact-form-7' ) ) {
+						echo do_shortcode( $this->getInput( 'contactFormShortCode' ) );
+					}
+					?>
+                </div>
             </div>
+            <div class="row back">
+                <h2>Thank you for message</h2>
         </div>
     </div>
 </div>
