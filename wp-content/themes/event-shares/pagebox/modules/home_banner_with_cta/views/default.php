b04ad3feaf9ed6e67b69be9fd7c921a5c754316c
mateuszkania <mateuszkania@protonmail.com>
2017-06-16 10:45:51 +0200

    EV-16 - change svg type in module

--- wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/views/default.php
@@ -6,72 +6,76 @@
  * @var \Nurture\EventShares\Module\HomeBanner $module
  */
 
-$module       = $this->getModule();
-$bgImage      = $this->getMedia( 'bgImage' )->getImage()->getUrl( 'normal' );
-$title        = $this->getInput( 'title' );
-$titleInner   = $this->getInput( 'titleInner' );
-$description  = $this->getEditor( 'description' )->getContent();
-$buttonLeft   = $this->getInput( 'buttonLeftTitle' );
-$buttonMiddle = $this->getInput( 'buttonMiddleTitle' );
-$buttonRight  = $this->getInput( 'buttonRightTitle' );
+$module = $this->getModule();
+$bgImage = $this->getMedia('bgImage')->getImage()->getUrl('normal');
+$title = $this->getInput('title');
+$titleInner = $this->getInput('titleInner');
+$description = $this->getEditor('description')->getContent();
+$buttonLeft = $this->getInput('buttonLeftTitle');
+$buttonMiddle = $this->getInput('buttonMiddleTitle');
+$buttonRight = $this->getInput('buttonRightTitle');
 ?>
 <div class="<?= $module->getClass() ?>">
 
     <div class="container">
         <?= createTaskLink('EV-16') ?>
-        <div class="row"">
+        <div class="row">
             <div class="col-lg-6 text-content">
                 <h2><?= $title ?><span id="header-second-color"><?= $titleInner ?></span></h2>
 
-				<?= $description ?>
-				<?php
-//                Left button
-				if ( $this->getInput( 'buttonLeftOn' )->getValue() )
-					:?>
-					<?php
-					$url   = $this->getSelect( 'buttonUrlLeft' )->getValue()['permalink'];
-					$blank = $this->getInput( 'buttonLeftBlankLink' )->getValue() ? 'target=_blank' : "";
-					?>
+                <?= $description ?>
+                <?php
+                //                Left button
+                if ($this->getInput('buttonLeftOn')->getValue())
+                    :?>
+                    <?php
+                    $url = $this->getSelect('buttonUrlLeft')->getValue()['permalink'];
+                    $blank = $this->getInput('buttonLeftBlankLink')->getValue() ? 'target=_blank' : "";
+                    ?>
                     <a href="<?= $url ?>" <?= $blank ?> class="btn btn-primary learn-more-button">
                         <span class="align-middle"><?= $buttonLeft ?></span>
                     </a>
-					<?php
-				endif;
-				?>
+                    <?php
+                endif;
+                ?>
 
-	            <?php
-//                Middle Button
-	            if ( $this->getInput( 'buttonMiddleOn' )->getValue() )
-		            :?>
-		            <?php
-		            $url   = $this->getSelect( 'buttonUrlMiddle' )->getValue()['permalink'];
-		            $blank = $this->getInput( 'buttonMiddleBlankLink' )->getValue() ? 'target=_blank' : "";
-		            ?>
+                <?php
+                //                Middle Button
+                if ($this->getInput('buttonMiddleOn')->getValue())
+                    :?>
+                    <?php
+                    $url = $this->getSelect('buttonUrlMiddle')->getValue()['permalink'];
+                    $blank = $this->getInput('buttonMiddleBlankLink')->getValue() ? 'target=_blank' : "";
+                    ?>
                     <a href="<?= $url ?>" <?= $blank ?> class="btn btn-primary learn-more-button">
                         <span class="align-middle"><?= $buttonMiddle ?></span>
                     </a>
-		            <?php
-	            endif;
-	            ?>
-	            <?php
-//                Right Button
-	            if ( $this->getInput( 'buttonRightOn' )->getValue() )
-		            :?>
-		            <?php
-		            $url   = $this->getSelect( 'buttonUrlRight' )->getValue()['permalink'];
-		            $blank = $this->getInput( 'buttonRightBlankLink' )->getValue() ? 'target=_blank' : "";
-		            ?>
+                    <?php
+                endif;
+                ?>
+                <?php
+                //                Right Button
+                if ($this->getInput('buttonRightOn')->getValue())
+                    :?>
+                    <?php
+                    $url = $this->getSelect('buttonUrlRight')->getValue()['permalink'];
+                    $blank = $this->getInput('buttonRightBlankLink')->getValue() ? 'target=_blank' : "";
+                    ?>
                     <a href="<?= $url ?>" <?= $blank ?> class="btn btn-primary learn-more-button">
                         <span class="align-middle"><?= $buttonRight ?></span>
                     </a>
-		            <?php
-	            endif;
-	            ?>
+                    <?php
+                endif;
+                ?>
             </div>
             <div class="col-lg-6 shapes">
                 <div class="shape-image">
                     <img src="<?= THEME_IMAGES_URI ?>/sample-image.jpg">
-                    <img class="style-svg image-punch" src="<?= THEME_IMAGES_URI ?>/punch-01.svg" alt="">
+                    <svg class="image-punch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 183.65 159.67">
+                        <title>punch</title>
+                        <path class="a"
+                              d="M182.12,0A10.1,10.1,0,0,1,181,5.76L104.55,151c-4.8,9.12-19.91,9.15-24.75,0L2.71,5.79A10.11,10.11,0,0,1,1.53,0H0V159.67H183.65V0Z"/>
+                    </svg>
                 </div>
 
             </div>
