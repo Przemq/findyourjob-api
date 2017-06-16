d89aad213e8dc619e8912ecad2c97b6f3e1864b2
Artur <artur@nurtureagency.com>
2017-06-16 15:58:00 +0200

    EV-37 - class changed and styles like headlines and colors

--- wp-content/themes/event-shares/pagebox/modules/search_results/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/search_results/views/default.php
@@ -9,77 +9,80 @@
 $module = $this->getModule();
 ?>
 <div class="<?= $module->getClass() ?>">
-    <div class="container-fluid" id="page-result-background">
-        <div class="container">
-            <?= createTaskLink('EV-37') ?>
-            <?php include_once('pageContent.php'); ?>
-            <div class="row" id="page-results">
-                <div id="single-result">
-                    <div class="col-lg-12"><h4 id="search-top-header"><?php echo $pageContent['page_results']; ?></h4>
+    <div class="page">
+        <div class="container-fluid">
+            <div class="container">
+                <?= createTaskLink('EV-37') ?>
+                <?php include_once('pageContent.php'); ?>
+                <div class="row">
+                    <div id="single-result">
+                        <div class="col-lg-12"><h3><?php echo $pageContent['page_results']; ?></h3>
+                        </div>
+                        <div class="single-result col-lg-9">
+                            <h4><?php echo $pageContent['result_header']; ?></h4>
+                            <p><?php echo $pageContent['result_paragraph']; ?><a
+                                        id="read-more"><?php echo $pageContent['read_more']; ?></a></p>
+                        </div>
                     </div>
-                    <div class="single-result col-lg-9">
-                        <h4><?php echo $pageContent['result_header']; ?></h4>
-                        <p><?php echo $pageContent['result_paragraph']; ?><a
-                                    id="read-more"><?php echo $pageContent['read_more']; ?></a></p>
+                    <div class="single-result">
+                        <div class="single-result col-lg-9">
+                            <h4><?php echo $pageContent['result_header']; ?></h4>
+                            <p><?php echo $pageContent['result_paragraph']; ?><a
+                                        id="read-more"><?php echo $pageContent['read_more']; ?></a></p>
+                        </div>
                     </div>
-                </div>
-                <div id="single-result">
-                    <div class="single-result col-lg-9">
-                        <h4><?php echo $pageContent['result_header']; ?></h4>
-                        <p><?php echo $pageContent['result_paragraph']; ?><a
-                                    id="read-more"><?php echo $pageContent['read_more']; ?></a></p>
-                    </div>
-                </div>
-                <div id="single-result">
-                    <div class="single-result col-lg-9">
-                        <h4><?php echo $pageContent['result_header']; ?></h4>
-                        <p><?php echo $pageContent['result_paragraph']; ?><a
-                                    id="read-more"><?php echo $pageContent['read_more']; ?></a></p>
+                    <div class="single-result">
+                        <div class="single-result col-lg-9">
+                            <h4><?php echo $pageContent['result_header']; ?></h4>
+                            <p><?php echo $pageContent['result_paragraph']; ?><a
+                                        id="read-more"><?php echo $pageContent['read_more']; ?></a></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="container-fluid" id="article-container">
-        <div class="container">
-            <div class="row" id="article-results">
-                <div class="col-lg-12"><h4 id="search-top-header">article results</h4>
-                </div>
-
-                <div class="col-lg-4 single-article">
-                    <div class="tile-wrapper">
-                        <img class="article-icon" src="<?= THEME_IMAGES_URI; ?>/ES%20Piggy%20Bank%20Icon-01-01.svg">
-                        <div class="publication-info col-lg-12">
-                            <?php echo $pageContent['article_date'] . ' | ' . $pageContent['article_author']; ?>
-                        </div>
-                        <div class="col-lg-12"><h4><?php echo $pageContent['article_title']; ?></h4></div>
-                        <div class="col-lg-12"><p><?php echo $pageContent['article_paragraph']; ?></p></div>
-                        <div class="col-lg-12" style="text-align: left"><a href="#">READ NOW</a></div>
+    <div class="article">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12"><h3>article results</h3>
                     </div>
-                </div>
 
-                <div class="col-lg-4 single-article">
-                    <div class="tile-wrapper">
-                        <img class="article-icon" src="<?= THEME_IMAGES_URI; ?>/ES%20Piggy%20Bank%20Icon-01-01.svg">
-                        <div class="publication-info col-lg-12">
-                            <?php echo $pageContent['article_date'] . ' | ' . $pageContent['article_author']; ?>
+                    <div class="col-lg-4 single-article">
+                        <div class="tile-wrapper">
+                            <img class="article-icon" src="<?= THEME_IMAGES_URI; ?>/ES%20Piggy%20Bank%20Icon-01-01.svg">
+                            <div class="publication-info col-lg-12">
+                                <?php echo $pageContent['article_date'] . ' | ' . $pageContent['article_author']; ?>
+                            </div>
+                            <div class="col-lg-12"><h4><?php echo $pageContent['article_title']; ?></h4></div>
+                            <div class="col-lg-12"><p><?php echo $pageContent['article_paragraph']; ?></p></div>
+                            <div class="col-lg-12" style="text-align: left"><a href="#">READ NOW</a></div>
                         </div>
-                        <div class="col-lg-12"><h4><?php echo $pageContent['article_title']; ?></h4></div>
-                        <div class="col-lg-12"><p><?php echo $pageContent['article_paragraph']; ?></p></div>
-                        <div class="col-lg-12" style="text-align: left"><a href="#">READ NOW</a></div>
                     </div>
-                </div>
 
-                <div class="col-lg-4 single-article">
-                    <div class="tile-wrapper">
-                        <img class="article-icon" src="<?= THEME_IMAGES_URI; ?>/ES%20Piggy%20Bank%20Icon-01-01.svg">
-                        <div class="publication-info col-lg-12">
-                            <?php echo $pageContent['article_date'] . ' | ' . $pageContent['article_author']; ?>
+                    <div class="col-lg-4 single-article">
+                        <div class="tile-wrapper">
+                            <img class="article-icon" src="<?= THEME_IMAGES_URI; ?>/ES%20Piggy%20Bank%20Icon-01-01.svg">
+                            <div class="publication-info col-lg-12">
+                                <?php echo $pageContent['article_date'] . ' | ' . $pageContent['article_author']; ?>
+                            </div>
+                            <div class="col-lg-12"><h4><?php echo $pageContent['article_title']; ?></h4></div>
+                            <div class="col-lg-12"><p><?php echo $pageContent['article_paragraph']; ?></p></div>
+                            <div class="col-lg-12" style="text-align: left"><a href="#">READ NOW</a></div>
                         </div>
-                        <div class="col-lg-12"><h4><?php echo $pageContent['article_title']; ?></h4></div>
-                        <div class="col-lg-12"><p><?php echo $pageContent['article_paragraph']; ?></p></div>
-                        <div class="col-lg-12" style="text-align: left"><a href="#">READ NOW</a></div>
+                    </div>
+
+                    <div class="col-lg-4 single-article">
+                        <div class="tile-wrapper">
+                            <img class="article-icon" src="<?= THEME_IMAGES_URI; ?>/ES%20Piggy%20Bank%20Icon-01-01.svg">
+                            <div class="publication-info col-lg-12">
+                                <?php echo $pageContent['article_date'] . ' | ' . $pageContent['article_author']; ?>
+                            </div>
+                            <div class="col-lg-12"><h4><?php echo $pageContent['article_title']; ?></h4></div>
+                            <div class="col-lg-12"><p><?php echo $pageContent['article_paragraph']; ?></p></div>
+                            <div class="col-lg-12" style="text-align: left"><a href="#">READ NOW</a></div>
+                        </div>
                     </div>
                 </div>
             </div>
