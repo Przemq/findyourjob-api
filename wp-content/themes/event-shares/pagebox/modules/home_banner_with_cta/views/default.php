ad51489657be3c116f8a2d8900d257aea187a3fb
mateuszkania <mateuszkania@protonmail.com>
2017-06-19 09:54:59 +0200

    EV-16 - add desktop parallax

--- wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/home_banner_with_cta/views/default.php
@@ -28,13 +28,11 @@ $buttonRight = $this->getInput('buttonRightTitle');
 
             <div class="col-lg-6 push-lg-6 shape">
                 <div class="shape-image">
-<!--                    <img src="--><?//= THEME_IMAGES_URI ?><!--/sample-image.jpg">-->
 
                     <div class="parallax-window" data-parallax="scroll" data-image-src="<?= THEME_IMAGES_URI ?>/sample-image.jpg"></div>
-
-                    <svg class="image-punch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 183.6 219.7"><title>punch-01</title><path d="M182.1,0A10.1,10.1,0,0,1,181,5.8L104.6,151c-4.8,9.1-19.9,9.1-24.8,0L2.7,5.8A10.1,10.1,0,0,1,1.5,0H0V219.7H183.6V0Z"/></svg>
+                    <svg class="image-punch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 263.6 219.7"><title>pu01</title><path d="M222.1,0A10.1,10.1,0,0,1,221,5.8L144.6,151c-4.8,9.1-19.9,9.1-24.7,0L42.7,5.8A10.1,10.1,0,0,1,41.5,0H0V219.7H263.6V0Z"/></svg>
                 </div>
-
+                <div class="parallax-mask-fix"></div>
             </div>
 
             <div class="col-lg-6 pull-lg-6 text-content">
