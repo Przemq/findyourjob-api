6ddb3172006649d34b05a8c78f5cdcd1c9d2314d
Piotr <pgurdek@gmail.com>
2017-06-27 23:56:04 +0200

    EV-33 - added slide on tab switch

--- wp-content/themes/event-shares/pagebox/modules/text_banner_and_subnav/js/module.js
+++ wp-content/themes/event-shares/pagebox/modules/text_banner_and_subnav/js/module.js
@@ -1,40 +1,31 @@
 'use strict';
 (function ($) {
-    var $window = $(window),
+    let $window = $(window),
         $thisModule = $(".wpx-mf3f354e1");
 
-    $thisModule.each(function (index, el) {
-        var $module = $(el),
-            $navDropdwon = $module.find('.nav-tabs-dropdown');
 
-        $navDropdwon.on('click', function (e) {
-            e.preventDefault();
-            $(e.target).toggleClass('show').next('ul').slideToggle(400);
-        });
+    $($thisModule).find('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        let target = $(this).attr('href');
+        $(target).css('left','-'+$(window).width()+'px');
+        let left = $(target).offset().left;
+        $(target).css({left:left}).animate({"left":"0px"}, "400");
+    })
 
-        $('.nav-tabs-wrapper a[data-toggle="tab"]').on('click', function (e) {
-            e.preventDefault();
-            $(e.target).closest('ul').hide().prev('a').removeClass('show').text($(this).text());
 
-        });
-    });
-    $thisModule.each(function (index, el) {
-        var $module = $(el),
-            $textPanel = $module.find('.text-content');
+    // $thisModule.each(function (index, el) {
+    //     var $module = $(el),
+    //         $navDropdwon = $module.find('.nav-tabs-dropdown');
+    //
+    //     $navDropdwon.on('click', function (e) {
+    //         e.preventDefault();
+    //         $(e.target).toggleClass('show').next('ul').slideToggle(400);
+    //     });
+    //
+    //     $('.nav-tabs-wrapper a[data-toggle="tab"]').on('click', function (e) {
+    //         e.preventDefault();
+    //         $(e.target).closest('ul').hide().prev('a').removeClass('show').text($(this).text());
+    //
+    //     });
+    // });
 
-        function debounce(func, wait, immediate) {
-            var timeout;
-            return function () {
-                var context = this, args = arguments;
-                var later = function () {
-                    timeout = null;
-                    if (!immediate) func.apply(context, args);
-                };
-                var callNow = immediate && !timeout;
-                clearTimeout(timeout);
-                timeout = setTimeout(later, wait);
-                if (callNow) func.apply(context, args);
-            };
-        };
-    });
 })(jQuery);
