5f8bad85840ce7900468c763c4eaac6e70f5378c
Piotr <pgurdek@gmail.com>
2017-07-19 12:59:38 +0200

    EV-123 - added mobile design

--- wp-content/themes/event-shares/pagebox/modules/team_module/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/team_module/views/default.php
@@ -15,12 +15,19 @@ $numberOfSections = count( $this->getRepeater( 'tabs' ) );
 // Sort new tab for repeater
 $sectionArray = array();
 $tempArray    = array();
+$sectionArrayMobile = array();
+$tempArrayMobile    = array();
 foreach ( $this->getRepeater( 'tabs' ) as $index => $value ) {
 	array_push( $tempArray, $value );
+	array_push( $tempArrayMobile, $value );
 	if ( ( $index + 1 ) % 4 === 0 || $index === $numberOfSections - 1 ) {
-
 		array_push( $sectionArray, $tempArray );
 		$tempArray = array();
+	}
+
+	if ( ( $index + 1 ) % 2 === 0 || $index === $numberOfSections - 1 ) {
+		array_push( $sectionArrayMobile, $tempArrayMobile );
+		$tempArrayMobile = array();
 	}
 }
 ?>
@@ -40,60 +47,118 @@ foreach ( $this->getRepeater( 'tabs' ) as $index => $value ) {
             </div>
         </div>
 
-        <!--        First Loop to dispaly first row-->
-		<?php
-		for ( $i = 0; $i < sizeof( $sectionArray ); $i ++ ): ?>
-            <ul class="team-nav nav nav-tabs row" role="tablist">
-				<?php $uniqeID = uniqid(); ?>
-				<?php foreach ( $sectionArray[ $i ] as $index => $value ): ?>
-					<?php
-					$startElementClass = "";
-					if ( $i == 0 && $index == 0 ) {
-						$startElementClass = "active";
-					}
+        <!--        Desktop  -->
+		<div class="hidden-sm-down">
+			<?php
+			for ( $i = 0; $i < sizeof( $sectionArray ); $i ++ ): ?>
+                <ul class="team-nav nav nav-tabs row" role="tablist">
+					<?php $uniqeID = uniqid(); ?>
+					<?php foreach ( $sectionArray[ $i ] as $index => $value ): ?>
+						<?php
+						$startElementClass = "";
+						if ( $i == 0 && $index == 0 ) {
+							$startElementClass = "active";
+						}
+						?>
+                        <li class="nav-item col-lg-3">
+                            <a data-index="<?= $index ?>" class="nav-link <?= $startElementClass ?>" data-toggle="tab"
+                               href="#tab-<?= $uniqeID . '' . $index ?>"
+                               role="tab"><?= $value->getInput( 'teamTitle' ); ?>
+                                <span><?= $value->getInput( 'jobTitle' ); ?></span></a>
+                        </li>
+
+
+					<?php endforeach;
 					?>
-                    <li class="nav-item col-3 col-lg-3">
-                        <a class="nav-link <?= $startElementClass ?>" data-toggle="tab"
-                           href="#tab-<?= $uniqeID . '' . $index ?>"
-                           role="tab"><?= $value->getInput( 'teamTitle' ); ?>
-                            <span><?= $value->getInput( 'jobTitle' ); ?></span></a>
-                    </li>
+
+                </ul>
+
+                <div class="tab-content">
+					<?php foreach ( $sectionArray[ $i ] as $index => $value ): ?>
+						<?php
+						$startElementClass = "";
+						if ( $i == 0 && $index == 0 ) {
+							$startElementClass = "active show";
+						}
+						?>
+                        <div class="tab-pane fade <?= $startElementClass ?> " id="tab-<?= $uniqeID . '' . $index ?>"
+                             role="tabpanel">
+                            <div class="quote">
+								<?= $value->getEditor( 'quote' )->getValue(); ?>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+									<?= $value->getEditor( 'leftDescriptionPanel' )->getValue(); ?>
+                                </div>
+                                <div class="col-12 col-lg-6 column">
+									<?= $value->getEditor( 'rightDescriptionPanel' )->getValue(); ?>
+                                </div>
+                            </div>
+                        </div>
 
 
-				<?php endforeach;
-				?>
-
-            </ul>
-
-            <div class="tab-content">
-				<?php foreach ( $sectionArray[ $i ] as $index => $value ): ?>
-					<?php
-					$startElementClass = "";
-					if ( $i == 0 && $index == 0 ) {
-						$startElementClass = "active show";
-					}
+					<?php endforeach;
 					?>
-                    <div class="tab-pane fade <?= $startElementClass ?> " id="tab-<?= $uniqeID . '' . $index ?>"
-                         role="tabpanel">
-                        <div class="quote">
-							<?= $value->getEditor( 'quote' )->getValue(); ?>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 col-lg-6">
-								<?= $value->getEditor( 'leftDescriptionPanel' )->getValue(); ?>
+
+                </div>
+			<?php endfor ?>
+        </div>
+        <div class="hidden-md-up">
+            <?php dump($sectionArrayMobile)?>
+		    <?php
+		    for ( $i = 0; $i < sizeof( $sectionArrayMobile ); $i ++ ): ?>
+                <ul class="team-nav nav nav-tabs row" role="tablist">
+				    <?php $uniqeID = uniqid(); ?>
+				    <?php foreach ( $sectionArrayMobile[ $i ] as $index => $value ): ?>
+					    <?php
+					    $startElementClass = "";
+					    if ( $i == 0 && $index == 0 ) {
+						    $startElementClass = "active";
+					    }
+					    ?>
+                        <li class="nav-item col-6">
+                            <a data-index="<?= $index ?>" class="nav-link <?= $startElementClass ?>" data-toggle="tab"
+                               href="#tab-<?= $uniqeID . '' . $index ?>"
+                               role="tab"><?= $value->getInput( 'teamTitle' ); ?>
+                                <span><?= $value->getInput( 'jobTitle' ); ?></span></a>
+                        </li>
+
+
+				    <?php endforeach;
+				    ?>
+
+                </ul>
+
+                <div class="tab-content">
+				    <?php foreach ( $sectionArrayMobile[ $i ] as $index => $value ): ?>
+					    <?php
+					    $startElementClass = "";
+					    if ( $i == 0 && $index == 0 ) {
+						    $startElementClass = "active show";
+					    }
+					    ?>
+                        <div class="tab-pane fade <?= $startElementClass ?> " id="tab-<?= $uniqeID . '' . $index ?>"
+                             role="tabpanel">
+                            <div class="quote">
+							    <?= $value->getEditor( 'quote' )->getValue(); ?>
                             </div>
-                            <div class="col-12 col-lg-6 column">
-								<?= $value->getEditor( 'rightDescriptionPanel' )->getValue(); ?>
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+								    <?= $value->getEditor( 'leftDescriptionPanel' )->getValue(); ?>
+                                </div>
+                                <div class="col-12 col-lg-6 column">
+								    <?= $value->getEditor( 'rightDescriptionPanel' )->getValue(); ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
 
-				<?php endforeach;
-				?>
+				    <?php endforeach;
+				    ?>
 
-            </div>
-		<?php endfor ?>
+                </div>
+		    <?php endfor ?>
+        </div>
 
     </div>
 
