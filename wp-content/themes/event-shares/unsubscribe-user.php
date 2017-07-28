091a88d6cb2b634fd3d6e9ed6cac37430aa76b2c
Przemek <przemyslawkudla94@gmail.com>
2017-07-28 12:28:31 +0200

    unsubscribe service

--- wp-content/themes/event-shares/unsubscribe-user.php
+++ wp-content/themes/event-shares/unsubscribe-user.php
@@ -1,12 +1,5 @@
 <?php
-$hash = $_GET['hash'];
-global $wpdb;
-$tableName = $wpdb->base_prefix . 'wpx_' . 'subscriptions';
-$results = $wpdb->query("DELETE FROM" . $tableName . "WHERE hash=" . $hash);
-
-if($results){
-    echo 'usunięto';
-}
-else {
-    echo 'error';
-}
+//$hash = $_GET['hash'];
+//global $wpdb;
+//$tableName = $wpdb->base_prefix . 'wpx_' . 'subscriptions';
+//$results = $wpdb->query("DELETE FROM" . $tableName . "WHERE hash=" . $hash);
