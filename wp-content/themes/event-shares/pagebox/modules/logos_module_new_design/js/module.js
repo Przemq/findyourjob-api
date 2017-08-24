f8b159327d912a514c3493b0e1fffe7e3ffa4d96
Przemek <przemyslawkudla94@gmail.com>
2017-08-24 15:19:47 +0200

    logos module update

--- wp-content/themes/event-shares/pagebox/modules/logos_module_new_design/js/module.js
+++ wp-content/themes/event-shares/pagebox/modules/logos_module_new_design/js/module.js
@@ -1,37 +1,47 @@
 $(document).ready(function(){
-    $('.slick-container').slick({
-        arrows:false,
-        dots: false,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                    infinite: true,
-                    dots: false
+
+    var $thisModule = $('.wpx-mc19972a5');
+
+
+    $thisModule.each(function(){
+
+        $(this).css('opacity', 1);
+
+        $(this).find('.slick-container').slick({
+            arrows:false,
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            autoplay: false,
+            autoplaySpeed: 4000,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
                 }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }
-        ]
+            ]
+        });
+
     });
 });
