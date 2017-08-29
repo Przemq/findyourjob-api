4be041bbcc3c9c32a09e25a4ecca2afd275fa4fa
Przemek <przemyslawkudla94@gmail.com>
2017-08-29 16:51:25 +0200

    404 page, created scss file

--- wp-content/themes/event-shares/404.php
+++ wp-content/themes/event-shares/404.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-    <h1>404 Not Found</h1>
+    <div class="container page-404">
+        <div class="row">
+            <div class="col-12">
+                <h2>404</h2>
+                <h4>Not Found</h4>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer();
