03316115be7adca5237533402d34dcd7a99fa265
Przemek <przemyslawkudla94@gmail.com>
2017-09-13 12:22:15 +0200

    styles update for home page

--- wp-content/themes/event-shares/pagebox/modules/video_and_text/views/default.php
+++ wp-content/themes/event-shares/pagebox/modules/video_and_text/views/default.php
@@ -23,7 +23,7 @@ $buttonText = $this->getInput('buttonText')->getValue();
     <div class="container-fluid">
         <div class="container">
             <?= createTaskLink('EV-243') ?>
-            <div class="row">
+            <div class="row video-and-text">
                 <div class="col-lg-7 col-12 video-container">
                             <iframe src="https://www.youtube.com/embed/<?= $videoURL ?>"></iframe>
                 </div>
