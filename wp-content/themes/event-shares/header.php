d13621299860a0ae93a6aec5e8f34a2f3c566d0e
Przemek <przemek.kudla@nurtureagency.com>
2017-10-04 12:45:34 +0200

    move google analyicts code to googlw-analytics.js file

--- wp-content/themes/event-shares/header.php
+++ wp-content/themes/event-shares/header.php
@@ -25,10 +25,12 @@
         $menu_font_family = '"Merriweather Italic",serif';
     } else if ($menu_font_family === 'roboto') {
         $menu_font_family = "'Roboto Condensed', sans-serif";
-    } ?>
+    }
 
-    <?php include 'includes/constatn_parts_options/header-footer-search-options.php'; ?>
+    include 'includes/constatn_parts_options/header-footer-search-options.php';
 
+    ?>
+    <script type="text/javascript" src="assets/javascripts/google-analytics.js"></script>
 </head>
 <body class="<?= wpx_body_class() ?>">
 
@@ -133,23 +135,5 @@
             </div>
         </div>
     </div>
-    <!-- Google Analytics -->
-    <script>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-99129127-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <!-- End Google Analytics -->
 </header>
 
